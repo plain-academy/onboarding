a327471b863e44df600f7805227125dfc1921344
piro <pro.piro@gmail.com>
2021-05-01 00:48:00 +0200

    exercise 001 result (#2)
    * exercise 001 result
    
    * dropped the -s / -size argument, now it accepts just integer value
    
    * fixed character list from string_uppercase+string.digits to characterset from RFC 4648 Base32 alphabet
    
    * typo, missing O
    
    * Update exercises/e001/results/propiro/outputBase32faster.py
    
    Co-authored-by: kucharskim <mikolaj@kucharski.name>
    
    * Update exercises/e001/results/propiro/outputBase32faster.py
    
    Co-authored-by: kucharskim <mikolaj@kucharski.name>
    
    * drop duplicate line
    
    * drop blank lines
    
    * requested fixes
    
    * reworked function call
    
    Co-authored-by: kucharskim <mikolaj@kucharski.name>

---
+++ exercises/e001/results/propiro/outputBase32faster.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import string
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description='base32 key generator script')
+parser.add_argument('size', const=32, default=32, type=int, help='size of key, default=32', nargs='?')
+caseGroup = parser.add_mutually_exclusive_group()
+caseGroup.add_argument('-u', action="store_true", help='use uppercase letters, default option', default=True)
+caseGroup.add_argument('-l', action="store_true", help='use lowercase letters', default=False)
+
+arg = parser.parse_args()
+
+arg.u = not arg.l
+
+def generateString(keySize, charCase):
+    keyGenerated = ""          
+    base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+    
+    for _ in range(keySize):
+        keyGenerated += (random.choice(base32chars))
+    if not charCase:
+        keyGenerated = keyGenerated.lower()
+    #print(keyGenerated, len(keyGenerated))
+    print (keyGenerated)
+
+generateString(arg.size, arg.u)
