3a5f8baf2cac6d213d4980dd228d6bf0afe541cb
piro <pro.piro@gmail.com>
2021-05-01 00:19:05 +0200

    Update exercises/e001/results/propiro/outputBase32faster.py
    Co-authored-by: kucharskim <mikolaj@kucharski.name>

--- exercises/e001/results/propiro/outputBase32faster.py
+++ exercises/e001/results/propiro/outputBase32faster.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 import string
 import random
 import argparse
@@ -29,6 +31,5 @@ def generateString(keySize =arg.size, charCase = arg.u, ):
      
 
 generateString()
-
 
 
