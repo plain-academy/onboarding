50f67a4912d52c4a9829bc29694e7611a4d4316e
Frydan <frycowskidan@gmail.com>
2021-05-01 18:21:09 -0400

    fixed all the requested things

--- exercises/e001/results/Frydan/b32gen.py
+++ exercises/e001/results/Frydan/b32gen.py
@@ -5,19 +5,23 @@ import random
 
 parser = argparse.ArgumentParser(description='Base32 (RFC 4648) random character string generator')
 meg = parser.add_mutually_exclusive_group()
-parser.add_argument('-c', '--characters', type=int, default=32, metavar='', help='Number of characters to generate (default: 32)')
+parser.add_argument('characters', type=int, default=32, nargs='?', help='Number of characters to generate (default: 32)')
 meg.add_argument('-l', '--lowercase', action='store_false', help='Output all lowercase (default choice)')
 meg.add_argument('-u', '--uppercase', action='store_true', help='Output all uppercase')
 
 args = parser.parse_args()
 
-def getString(numOfCharacters, notLowercase):	# main function for generating base32 strings
+# main function for generating base32 strings
+def getString(numOfCharacters, notLowercase):
 	buff = ''
-	for i in range(numOfCharacters):
+	for _ in range(numOfCharacters):
 		rnd = random.randrange(0, 32)
-		if rnd < 26:				# A (0) - Z (25) Characters
+		# A (0) - Z (25) Characters
+		if rnd < 26:
 			buff += chr(rnd+65)
-		else:						# 2 (26) - 7 (31) Characters, 0 and 1 are skipped for RFC 4648
+		# 2 (26) - 7 (31) Characters, 
+		# 0 and 1 are skipped for RFC 4648
+		else:
 			buff += chr(rnd+24)
 	if (notLowercase):
 		return buff
