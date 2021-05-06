c71ce65fce3da4f0808d0a3fe5044136f4be8e08
MikJarz <mik.jarz00@gmail.com>
2021-05-06 16:01:39 +0200

    Added same variable for both options

--- exercises/e001/results/mikjarz/e001.py
+++ exercises/e001/results/mikjarz/e001.py
@@ -5,23 +5,20 @@ import argparse
 parser = argparse.ArgumentParser(description="Random string of base32 characters")
 parser.add_argument('characters', default=32, type=int, nargs='?', help='How many characters to print, default=32')
 caseGroup = parser.add_mutually_exclusive_group()
-parser.add_argument('-u', '--upper', action='store_true', help='Printing all upper characters')
-parser.add_argument('-l', '--lower', action='store_true', help='Printing all lower characters')
+parser.add_argument('-l', '--lower', action='store_true', dest='size', help='Printing all lower characters')
+parser.add_argument('-u', '--upper', action='store_false', dest='size', help='Printing all upper characters')
 
 args = parser.parse_args()
 
 
 def random_string(n):
-    r = random.randint(0, 255)
     a = [random.randint(0, 255) for _ in range(n)]
     b = bytes(a)
     z = base64.b32encode(b)
-    if args.upper:
-        return z.upper()
-    elif args.lower:
+    if args.size:
         return z.lower()
     else:
-        return z
+        return z.upper()
 
 
 if __name__ == '__main__':
