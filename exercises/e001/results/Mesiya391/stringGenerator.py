79e5b200a719fd3109e9186cc481b3e4831a8279
CFAPL.CG.DEVOPS.DELTA <CFAPL.CG.DEVOPS@accenture.com>
2021-05-08 20:16:48 +0200

    changed string.ascii_uppercase to base32 and used dest from argparse

--- exercises/e001/results/Mesiya391/stringGenerator.py
+++ exercises/e001/results/Mesiya391/stringGenerator.py
@@ -1,21 +1,22 @@
 import argparse
-import string
 import random
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=False)
 parser.add_argument("size", help="sets length of chain", nargs='?', const=32, type=int, default=32)
-group.add_argument("-l", "--lowercase",  help="set letters to lowercase",  action='store_true')
-group.add_argument("-u", "--uppercase", help = "set letters to uppercase", action='store_true')
+group.add_argument("-l", "--lowercase",  help="set letters to lowercase",  action='store_true', dest="case")
+group.add_argument("-u", "--uppercase", help = "set letters to uppercase", action='store_false', dest='case')
 args = parser.parse_args()
 
+base32 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
 def genString(size, toLower=False):
-	if toLower==True:
-		output = ''.join(random.choice(string.ascii_uppercase) for _ in range(size)).lower()
+	if toLower!=True:
+		output = ''.join(random.choice(base32) for _ in range(size))
 	else:
-		output = ''.join(random.choice(string.ascii_uppercase) for _ in range(size))
+		output = ''.join(random.choice(base32) for _ in range(size)).lower()
 	print(output)
 
-genString(args.size, args.lowercase)
+genString(args.size, args.case)
 
 
