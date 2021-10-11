47a207d3701f5533d0e85f8f9e2bfb2d6c9dc73a
ukrainizator <deep7d.ks@gmail.com>
2021-10-11 15:09:20 +0200

    Create code.py

---
+++ exercises/e001-pwgen/results/ukrainizator/code.py
@@ -0,0 +1,20 @@
+# Importing random to generate
+# random string sequence
+import random
+  
+# Importing string library function
+import string
+  
+def rand_pass(size):
+      
+    # Takes random choices from
+    # ascii_letters and digits BASE32
+    generate_pass = ''.join([random.choice(
+                        string.ascii_letters + string.digits)
+                        for n in range(size)])
+                          
+    return generate_pass
+  
+# Driver Code 
+password = rand_pass(7) #change length of generated string
+print(password.upper()) #print all symbols in uppercase
